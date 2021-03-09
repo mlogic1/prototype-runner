8c9d8b56132b6bf9b186fcc70e57fd08a0f28c01
mlogic1 <multilogic@outlook.com>
2021-03-09 18:16:34 +0100

    added camera behaviour

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform = null;
+	public float cameraDistanceFromPlayer = 20.0f;
+	public float cameraHeightAbovePlayer = 6.0f;
+	public float smoothCamera = 1.125f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+	void LateUpdate()
+	{
+		if (playerTransform == null)
+		{
+			Debug.LogWarning("Player reference not set in CameraController");
+			return;
+		}
+
+		Vector3 lerpPoint = new Vector3(playerTransform.position.x, playerTransform.position.y + cameraHeightAbovePlayer, playerTransform.position.z - cameraDistanceFromPlayer);
+		transform.position = Vector3.Lerp(this.transform.position, lerpPoint, smoothCamera * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public Rigidbody playerRigidBody = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRigidBody == null)
+		{
+            return;
+		}
+
+        if (Input.GetKey(KeyCode.W))
+		{
+
+		}
+    }
+}
