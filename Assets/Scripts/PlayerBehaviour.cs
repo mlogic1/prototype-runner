9de1e65a16a6dbc1a3b13cacd6ff888c3646230c
mlogic1 <multilogic@outlook.com>
2021-03-09 20:32:31 +0100

    updated player behaviour and prefab, added more floors to the map

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,19 +7,27 @@ public class PlayerBehaviour : MonoBehaviour
     public float playerJumpStrenght = 200.0f;
     public float playerRunningSpeed = 20.0f;
     private Rigidbody playerRigidBody = null;
+
+    public float playerLaneSwitchSpeed = 10.0f;
+    private int playerLaneIndex = 1;
+    public List<Transform> runningLanes;
     
+    public float laneSwitchCooldown = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
         playerRigidBody = GetComponent<Rigidbody>();
+
+        if (runningLanes.Count != 3)
+		{
+            Debug.LogError("Lanes not sorted properly");
+		}
     }
 
     // Update is called once per frame
     void Update()
     {
-        // transform.position += transform.forward * playerRunningSpeed * Time.deltaTime;
-
-
         if (playerRigidBody == null)
 		{
             Debug.LogWarning("RigidBody component not found on PlayerBehaviour");
@@ -30,5 +38,36 @@ public class PlayerBehaviour : MonoBehaviour
 		{
             playerRigidBody.AddForce(new Vector3(0.0f, playerJumpStrenght), ForceMode.Impulse);
 		}
+
+        laneSwitchCooldown -= Time.deltaTime;
+        if (laneSwitchCooldown < 0.0f)
+        {
+            laneSwitchCooldown = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && laneSwitchCooldown < 0.1f)
+		{
+            playerLaneIndex--;
+            laneSwitchCooldown += 0.35f;
+            if (playerLaneIndex < 0)
+			{
+                playerLaneIndex = 0;
+			}
+		}
+
+        if (Input.GetKeyDown(KeyCode.D) && laneSwitchCooldown < 0.1f)
+        {
+            playerLaneIndex++;
+            laneSwitchCooldown += 0.35f;
+            if (playerLaneIndex > 2)
+            {
+                playerLaneIndex = 2;
+			}
+        }
+
+
+        Transform targetLane = runningLanes[playerLaneIndex];
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetLane.position.x, playerLaneSwitchSpeed * Time.deltaTime), transform.position.y, transform.position.z);    // lerp left and right
+        transform.position += transform.forward * playerRunningSpeed * Time.deltaTime;  // move forward
     }
 }
