63dabee8aef1edc5cf21b32a440d87264676cc6c
mlogic1 <multilogic@outlook.com>
2021-03-09 18:45:05 +0100

    updated player behaviour

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,25 +4,31 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public Rigidbody playerRigidBody = null;
+    public float playerJumpStrenght = 200.0f;
+    public float playerRunningSpeed = 20.0f;
+    private Rigidbody playerRigidBody = null;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // transform.position += transform.forward * playerRunningSpeed * Time.deltaTime;
+
+
         if (playerRigidBody == null)
 		{
+            Debug.LogWarning("RigidBody component not found on PlayerBehaviour");
             return;
 		}
 
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.Space))
 		{
-
+            playerRigidBody.AddForce(new Vector3(0.0f, playerJumpStrenght), ForceMode.Impulse);
 		}
     }
 }
