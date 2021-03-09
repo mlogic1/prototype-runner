6b9e052828a475351a192f4190707f8735e34947
mlogic1 <multilogic@outlook.com>
2021-03-09 23:28:34 +0100

    added obstacles

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,96 +5,103 @@ using UnityEngine.SceneManagement;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-  private const float LANE_SWITCH_COOLDOWN = 0.3f;
+    private const float LANE_SWITCH_COOLDOWN_INCREMENT = 0.3f;
 
-  public float playerJumpStrength;
-  public float playerRunningSpeed = 20.0f;
-  private Rigidbody playerRigidBody = null;
+    public float playerJumpStrength;
+    public float playerRunningSpeed = 20.0f;
+    private Rigidbody playerRigidBody = null;
 
-  public float playerLaneSwitchSpeed = 10.0f;
-  private int playerLaneIndex = 1;
-  public List<Transform> runningLanes;
+    public float playerLaneSwitchSpeed = 10.0f;
+    private int playerLaneIndex = 1;
+    public List<Transform> runningLanes;
 
-  public Transform WinScreen;
-  public Transform LoseScreen;
+    public Transform WinScreen;
+    public Transform LoseScreen;
 
-  private float laneSwitchCooldown = 0.0f;
+    private float laneSwitchCooldown = 0.0f;
 
-  private void OnTriggerEnter(Collider other)
-  {
-    if (other.tag.Equals("DeathFloor"))
+    private void OnTriggerEnter(Collider other)
     {
-      ShowLoseScreen();
-    }
-    else if (other.tag.Equals("Finish"))
-    {
-      ShowWinScreen();
-    }
-  }
-
-  private void ShowLoseScreen()
-  {
-    LoseScreen.gameObject.SetActive(true);
-  }
-
-  private void ShowWinScreen()
-  {
-    WinScreen.gameObject.SetActive(true);
-  }
-
-  // Start is called before the first frame update
-  void Start()
-  {
-    playerRigidBody = GetComponent<Rigidbody>();
-
-    if (runningLanes.Count != 3)
-    {
-      Debug.LogError("Lanes not sorted properly");
-    }
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    if (!GameController.GameControllerInstance.gameState.Equals(GameState.Active))
-    {
-      return;
+        if (other.tag.Equals("DeathFloor"))
+        {
+            ShowLoseScreen();
+        }
+        else if (other.tag.Equals("Finish"))
+        {
+            ShowWinScreen();
+        }
     }
 
-    if (playerRigidBody == null)
+    private void ShowLoseScreen()
     {
-      Debug.LogWarning("RigidBody component not found on PlayerBehaviour");
-      return;
+        GameController.GameControllerInstance.gameState = GameState.PostGameLose;
+        LoseScreen.gameObject.SetActive(true);
     }
 
-    laneSwitchCooldown -= Time.deltaTime;
-    if (laneSwitchCooldown < 0.0f)
+    private void ShowWinScreen()
     {
-      laneSwitchCooldown = 0.0f;
+        GameController.GameControllerInstance.gameState = GameState.PostGameWin;
+        WinScreen.gameObject.SetActive(true);
     }
 
-    if (Input.GetKeyDown(KeyCode.A) && laneSwitchCooldown < 0.01f)
+    // Start is called before the first frame update
+    void Start()
     {
-      playerLaneIndex--;
-      laneSwitchCooldown += LANE_SWITCH_COOLDOWN;
-      if (playerLaneIndex < 0)
-      {
-        playerLaneIndex = 0;
-      }
+        playerRigidBody = GetComponent<Rigidbody>();
+
+        if (runningLanes.Count != 3)
+        {
+            Debug.LogError("Lanes not setup properly");
+        }
     }
 
-    if (Input.GetKeyDown(KeyCode.D) && laneSwitchCooldown < 0.01f)
+    // Update is called once per frame
+    void Update()
     {
-      playerLaneIndex++;
-      laneSwitchCooldown += LANE_SWITCH_COOLDOWN;
-      if (playerLaneIndex > 2)
-      {
-        playerLaneIndex = 2;
-      }
+        if (!GameController.GameControllerInstance.gameState.Equals(GameState.Active))
+        {
+            return;
+        }
+
+        if (playerRigidBody == null)
+        {
+            Debug.LogWarning("RigidBody component not found on PlayerBehaviour");
+            return;
+        }
+
+        laneSwitchCooldown -= Time.deltaTime;
+        if (laneSwitchCooldown < 0.0f)
+        {
+            laneSwitchCooldown = 0.0f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && laneSwitchCooldown < 0.01f)
+        {
+            playerLaneIndex--;
+            if (playerLaneIndex < 0)
+            {
+                playerLaneIndex = 0;
+                IncreaseCooldown();
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D) && laneSwitchCooldown < 0.01f)
+        {
+            playerLaneIndex++;
+            if (playerLaneIndex > 2)
+            {
+                playerLaneIndex = 2;
+            }
+            IncreaseCooldown();
+        }
+
+        Transform targetLane = runningLanes[playerLaneIndex];
+        transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetLane.position.x, playerLaneSwitchSpeed * Time.deltaTime), transform.position.y, transform.position.z);    // lerp left and right
+        transform.position += transform.forward * playerRunningSpeed * Time.deltaTime;  // move forward
     }
 
-    Transform targetLane = runningLanes[playerLaneIndex];
-    transform.position = new Vector3(Mathf.Lerp(transform.position.x, targetLane.position.x, playerLaneSwitchSpeed * Time.deltaTime), transform.position.y, transform.position.z);    // lerp left and right
-    transform.position += transform.forward * playerRunningSpeed * Time.deltaTime;  // move forward
-  }
+    private void IncreaseCooldown()
+	{
+        laneSwitchCooldown += LANE_SWITCH_COOLDOWN_INCREMENT;
+    }
 }
